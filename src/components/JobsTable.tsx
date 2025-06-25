2002c97d0c295a974a8e0786127e1f1207398040
Pedro Martins <pedromartins@sl-WRCWFW2702.local>
2025-06-25 19:47:05 +0100

    Remove unused imports to fix Vercel build errors

--- src/components/JobsTable.tsx
+++ src/components/JobsTable.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import { Plus, SearchLg, Settings01, SwitchVertical01, DotsHorizontal, ChevronDown, FilterFunnel01 } from '@untitled-ui/icons-react';
 import Drawer from './Drawer';
-import SubNavigation from './SubNavigation';
 import Button from '@/components/Button';
 
 const jobs = [
